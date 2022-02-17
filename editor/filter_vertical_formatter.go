845f22c9daf986019bfc65221124f8b1e8a21eb2
Masayuki Morita <minamijoyo@gmail.com>
2022-02-17 10:20:34 +0900

    Expose low-level operations for vertical format

--- editor/filter_vertical_formatter.go
+++ editor/filter_vertical_formatter.go
@@ -34,15 +34,15 @@ func (f *verticalFormatterFilter) Filter(inFile *hclwrite.File) (*hclwrite.File,
 
 // VerticalFormat formats token in vertical.
 func VerticalFormat(tokens hclwrite.Tokens) hclwrite.Tokens {
-	trimmed := trimLeadingNewLine(tokens)
-	removed := removeDuplicatedNewLine(trimmed)
+	trimmed := TrimLeadingNewLine(tokens)
+	removed := RemoveDuplicatedNewLine(trimmed)
 	return removed
 }
 
-// trimLeadingNewLine trims leading newlines from tokens.
+// TrimLeadingNewLine trims leading newlines from tokens.
 // We don't need trim trailing newlines because the last newline should be
 // kept and others are removed removeDuplicatedNewLine.
-func trimLeadingNewLine(tokens hclwrite.Tokens) hclwrite.Tokens {
+func TrimLeadingNewLine(tokens hclwrite.Tokens) hclwrite.Tokens {
 	begin := 0
 	for ; begin < len(tokens); begin++ {
 		if tokens[begin].Type != hclsyntax.TokenNewline {
@@ -53,11 +53,11 @@ func trimLeadingNewLine(tokens hclwrite.Tokens) hclwrite.Tokens {
 	return tokens[begin:]
 }
 
-// removeDuplicatedNewLine removes duplicated newlines
+// RemoveDuplicatedNewLine removes duplicated newlines
 // Two consecutive blank lines should be removed.
 // In other words, if there are three consecutive TokenNewline tokens,
 // the third and subsequent TokenNewline tokens are removed.
-func removeDuplicatedNewLine(tokens hclwrite.Tokens) hclwrite.Tokens {
+func RemoveDuplicatedNewLine(tokens hclwrite.Tokens) hclwrite.Tokens {
 	var removed hclwrite.Tokens
 	beforeBefore := false
 	before := false
